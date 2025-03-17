964e352f9e48281c9df76792cbf7c6e10598a131
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-03-17 00:09:59 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+let BurgerNumber = sprites.create(img`
+    . . . . c c c b b b b b . . . . 
+    . . c c b 4 4 4 4 4 4 b b b . . 
+    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+    . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+    e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+    . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+    8 7 2 e e e e e e e e e e 2 7 8 
+    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+    e b e 8 8 c c 8 8 c c c 8 e b e 
+    e e b e c c e e e e e c e b e e 
+    . e e b b 4 4 4 4 4 4 4 4 e e . 
+    . . . c c c c c e e e e e . . . 
+    `, SpriteKind.Player)
+game.splash("Enter any number", "between 1 and 100")
+let number = game.askForNumber("Your number:", 100)
+if (number >= 1 && number <= 100) {
+    if (number % 3 == number % 5) {
+        BurgerNumber.sayText("FizzBuzz")
+    } else if (number % 3 == 0) {
+        BurgerNumber.sayText("Fizz")
+    } else if (number % 5 == 0) {
+        BurgerNumber.sayText("Buzz")
+    } else {
+        BurgerNumber.sayText(number)
+    }
+} else {
+    game.splash("Number not between 1 and 100")
+}
